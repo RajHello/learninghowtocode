5e770e2c0ffdc7d501e213b6fca9dd263d8c2a72
Raj Parmar <rajparmar182006@gmail.com>
2025-05-29 19:17:09 +0530

    pretty lame code

---
+++ additionofnos.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main(){
+
+    // we are gonna add numbs
+    int num;
+
+    printf("Enter The Number Jidhar Tak Add Karna hai:\n");
+    scanf("%d",&num);
+    int sum=0;
+    int i=1;
+
+
+    while(i<=num){
+        sum +=i;
+        i++;
+    }
+
+    printf("sum of your desired value is %d",sum);
+
+    return 0;
+}
