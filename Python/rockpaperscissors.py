bb31714a1c260f34233ed7ad3a869aa582e496f5
Quantumflux07 <soriaalpa@gmail.com>
2026-03-01 15:17:26 +0530

    Update rockpaperscissors.py. added multiple rounds and more ..
    added 3 round system + A special message for player if he lose + invalid input msg for wrong input

--- Python/rockpaperscissors.py
+++ Python/rockpaperscissors.py
@@ -1,4 +1,5 @@
 import random
+
 rock = '''
     _______
 ---'   ____)
@@ -26,31 +27,56 @@ scissors = '''
 ---.__(___)
 '''
 
-choice=[rock,paper,scissors]
+choices = [rock, paper, scissors]
+names = ["Rock ", "Paper ", "Scissors "]
 
-user=int(input("Choose 0:stone,1:paper,2:scissors:"))
+user_score = 0
+bot_score = 0
 
-if user>=0 and user<=2:
-    print("You Choose:",choice[user])
+print("First to 3 wins becomes the CHAMPION!\n")
 
-bot=random.randint(0,2)
-print("Computer Chose:")
-print(choice[bot])
+while user_score < 3 and bot_score < 3:
+    
+    try:
+        user = int(input("Choose 0: Rock, 1: Paper, 2: Scissors: "))
+    except:
+        print("Please enter a number (0, 1, or 2)\n")
+        continue
 
-if user>2 or user<0:
-    print("INVALID INPUT YOU LOSE")
+    if user < 0 or user > 2:
+        print("Invalid choice! Try again.\n")
+        continue
 
-if user==bot:
-    print("Its A DRAW")
-elif user==0 and bot==1:
-    print("YOU LOSE")
-elif user==1 and bot==0:
-    print("YOU WIN")
-elif user==2 and bot==1:
-    print("YOU WIN")
-elif user==1 and bot==2:
-    print("YOU Lose")
-elif user==0 and bot==2:
-    print("YOU WIN")
-elif user==2 and bot==0:
-    print("YOU LOSE")
+    print(f"\nYou chose {names[user]}")
+    print(choices[user])
+
+    print("Computer is choosing...")
+
+
+    bot = random.randint(0, 2)
+    print(f"Computer chose {names[bot]}")
+    print(choices[bot])
+
+    if user == bot:
+        print("It's a DRAW!\n")
+
+    elif (user == 0 and bot == 2) or \
+         (user == 1 and bot == 0) or \
+         (user == 2 and bot == 1):
+        print("You WIN this round!\n")
+        user_score += 1
+
+    else:
+        print("You LOSE this round!\n")
+        bot_score += 1
+
+    print(f"Score -> You: {user_score} | Computer: {bot_score}")
+    print("-" * 40)
+
+
+if user_score == 3:
+    print(" CONGRATULATIONS! YOU ARE THE CHAMPION! ")
+else:
+    print("BRO… uninstall the game. This isn’t for you 💀😂")
+    print("That wasn’t a loss… that was a public humiliation 😭🔥")
+
